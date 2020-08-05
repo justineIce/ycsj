9ae6abac4361660489fa1e1f425268893a449052
mamie <1306703963@qq.com>
2020-08-05 22:12:44 +0800

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:ycsj/util/theme.dart';
+import 'dart:io';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(new MyApp());
+  if (Platform.isAndroid) {
+    // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
+    SystemUiOverlayStyle systemUiOverlayStyle =
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  // 主题
+  Color themeColor = ThemeUtils.currentTheme;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Welcome to Flutter',
+      title: '英才私教',
+      theme:
+          new ThemeData(primaryColor: themeColor, brightness: Brightness.light),
+      // routes: '路由',
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text('Welcome to Flutter'),
---
+++ lib/util/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ThemeUtils {
+  // 默认主题色
+  static const Color defaultColor = Colors.redAccent;
+
+  // 当前的主题色
+  static Color currentTheme = defaultColor;
+}
